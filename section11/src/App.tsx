f292f6a878117f2c9d0bed618cd44d4f8bcc4608
s0zzang <dlth8888@gmail.com>
2024-05-27 13:44:50 +0900

    feat. Todo List 완료 - 기본형

--- section11/src/App.tsx
+++ section11/src/App.tsx
@@ -1,7 +1,35 @@
+import { todo } from "node:test";
+import { useState, useRef, useEffect } from "react";
 import "./App.css";
+import Editor from "./components/Editor";
+import TodoItem from "./components/TodoItem";
+import { Todo } from "./types";
 
 function App() {
-  return <div className="App"></div>;
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const idRef = useRef(0);
+
+  const onClickAdd = (text: string) => {
+    setTodos([...todos, { id: idRef.current++, content: text }]);
+  };
+
+  const onClickDelete = (id: number) => [
+    setTodos(todos.filter((todo) => todo.id !== id)),
+  ];
+
+  return (
+    <div className="App">
+      <h1>Todo</h1>
+      <Editor onClickAdd={onClickAdd}>
+        <div>child</div>
+      </Editor>
+      <div>
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} {...todo} onClickDelete={onClickDelete} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
